79582588d3911d2cb343cb2a64e641699b9b158f
walidmeguenni <walidmg125@gmail.com>
2021-09-19 16:36:06 +0100

    sparate morden landing page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+import{About,FAQ,Feature,Fotter,Pricing,Main,Testimonials,Works} from "./Layout";
+import Nav from "./components/nav"
+const App =()=>{
+    return (
+      <>
+      <Nav/>
+      <Main/>
+      <About/>
+      <Feature/>  
+      <Works/>
+      <Pricing/>
+      <Testimonials/>
+      <FAQ/>
+      <Fotter/>
+      </>
+    );
 }
 
 export default App;
